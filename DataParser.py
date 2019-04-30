981a8f1143af232f3ecfcf4ac8c1cbc5381d7fde
Manato1fg <manato1fg@gmail.com>
2019-04-30 21:36:28 +0900

    Initial commit

---
+++ DataParser.py
@@ -0,0 +1,62 @@
+"""
+Overview:
+DataParser.py gets divided words list from given input.
+this uses Yahoo Developers API which divide sentences into words and shows what kind of words they are.
+This outputs keywords in given sentences which are correspond to main keywords such as group name.
+
+@usage DataParser.py -input "data.csv"
+@license MIT
+@author Manato1fg
+"""
+
+import argparse
+import urllib.request
+import urllib.parse
+import xml.etree.ElementTree as ET
+import csv
+
+
+def main():
+    input_file = input_parse()
+    ary = []
+    ary.append(["id","name","description","keywords","url"])
+    base_url = "https://jlp.yahooapis.jp/MAService/V1/parse?appid=dj00aiZpPTN1U1QzQzh5aVNBUSZzPWNvbnN1bWVyc2VjcmV0Jng9MzA-&results=ma,uniq&uniq_filter=9%7C10&sentence="
+    with open(input_file, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+
+        for row in reader:
+            o_id, o_composition, o_name, o_description, o_url = row
+            s = o_composition + ":" + o_name + ":"
+            res = [o_id, o_name, o_description]
+            url = base_url + urllib.parse.quote(o_description)
+            req = urllib.request.Request(url)
+            with urllib.request.urlopen(req) as response:
+                XmlData = response.read()
+                root = ET.fromstring(XmlData)
+                for child1 in root:
+                    if child1.tag == '{urn:yahoo:jp:jlp}uniq_result':
+                        for child2 in child1:
+                            if child2.tag == '{urn:yahoo:jp:jlp}word_list':
+                                for child3 in child2:
+                                    for child4 in child3:
+                                        if child4.tag == '{urn:yahoo:jp:jlp}surface':
+                                            s += child4.text + ":"
+                res.append(s[0:-1])
+                res.append(o_url)
+                ary.append(res)
+    with open('output.csv', 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(ary)
+
+
+def input_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-input", required=True)
+    args = parser.parse_args()
+
+    return args.input
+
+
+if __name__ == "__main__":
+    main()
---
+++ server/concierge.php
@@ -0,0 +1,77 @@
+<?php
+
+const FILE_NAME = "./output.csv";
+
+if ( ! isset( $_POST["query"] ) ) {
+    echo "Invalid Argument";
+    return;
+}
+
+$data = [];
+
+const TEMPLATE_FOR_SERACH_NUM = "お探しの情報は%d件見つかりました。";
+const TEMPLATE_FOR_NO_FOUND = "お探しの情報は見つかりませんでした。キーワードを変えるか、減らして再度お試しください";
+const TEMPLATE_FOR_INFO = "%s<br><br>%s<br><a href='%s'>リンクはこちら</a>";
+
+$query = (string) urldecode($_POST["query"]);
+$query = str_replace("\n", "", $query);
+
+if(strpos($query, "@q") !== FALSE){
+    $query = str_replace("@q ", "@q", $query);
+    $query = str_replace("@q", "", $query);
+
+    $keywords = explode(" ", $query);
+
+    $result_array = [];
+    $cache = [];
+
+    $file_contents = file_get_contents(FILE_NAME);
+    $cache = explode("\n", $file_contents);
+    $cache2 = [];
+
+    foreach($cache as $row){
+        list($o_id, $o_name, $o_description, $o_keywords, $o_url) = explode(",", $row);
+        $cache2[$o_id] = [];
+        $cache2[$o_id] = ["name" => $o_name, "description" => $o_description, "url" => $o_url, "keywords" => $o_keywords];
+        $result_array[$o_id] = 0;
+    }
+
+    foreach($keywords as $keyword){
+        if($keyword === "") continue;
+        foreach($cache2 as $_o_id_ => $o_data){
+            $result_array[$_o_id_] += strpos($o_data["keywords"], $keyword) !== FALSE ? 1 : 0;
+        }
+    }
+
+    arsort($result_array);
+
+    $i = 0;
+    $max = 5;
+    $data[0] = "";
+    foreach($result_array as $key => $value){
+        if($value === 0) break;
+        $data[$i + 1] = sprintf(TEMPLATE_FOR_INFO, $cache2[$key]["name"], $cache2[$key]["description"], $cache2[$key]["url"]);
+        $i++;
+    }
+    if($i === 0){
+        $data[0] = TEMPLATE_FOR_NO_FOUND;
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        return;
+    }
+    $data[0] = sprintf(TEMPLATE_FOR_SERACH_NUM, $i);
+    echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    return;
+}else{
+
+    $keywords = json_decode(file_get_contents("./keywords.json"), true);
+    if(isset($keywords[$query])){
+        $data[0] = $keywords[$query];
+        echo json_encode($data, JSON_UNESCAPED_UNICODE); 
+    }else{
+        $rand_ary = json_decode(file_get_contents("./randomReply.json"), true);
+        $res = array_rand($rand_ary);
+        $data[0] = $rand_ary[$res];
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+}
+
