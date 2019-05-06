b92bde24c93220d9ffea4cfa8f464d4c20b17885
Manato <manato2cc@gmail.com>
2019-05-06 13:11:18 +0900

    流石に消しておこう

--- server/reply.php
+++ server/reply.php
@@ -2,15 +2,10 @@
 
 
 $rand_ary = [
-    "本日はお越しいただきありがとうございます！",
-    "翠実総務に入るにはユニークな試験を通過しなくてはいけません",
-    "翠嵐高校の先生方は個性に溢れています",
-    "なにやら、73期翠実総務には家まで上履きで帰ってしまった人がいるとか..."
 ];
 
 $keywords_ary = [
-    "うんち" => "うんち大好き！！",
-    "Twitter" => "翠翔祭のTwitterは<a href='https://twitter.com/suishosai0123'>@suishosai123</a>なのです！",
+
 ];
 
 $r_str = json_encode($rand_ary, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
